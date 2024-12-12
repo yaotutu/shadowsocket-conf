38091ffd4adf03987a8340dcdfd5719cf57b9885
yaotutu <957810668@qq.com>
2024-12-12 11:43:04 +0800

    chore: Modify request header in Coolapk-Scripts.js
    This commit modifies the request header in the Coolapk-Scripts.js file. The previous code used `$httpClient.setRequestHeader()` to set the "Accept" header, but it has been updated to use `$request.headers` instead. The modified header is then printed for debugging purposes. This change ensures that the image header issue is fixed and the script properly ends and returns the processed body data.

--- Coolapk-Scripts.js
+++ Coolapk-Scripts.js
@@ -2,12 +2,8 @@
 // 在脚本开始时输出调试信息
 console.log("Coolapk Image Header Fix script is running!");
 
-// 设置请求头
-$httpClient.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
+// 修改请求头
+$request.headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
 
 // 打印修改后的请求头，方便调试
 console.log($request.headers);
-
-// 结束脚本，返回处理后的 body 数据
-// 注意：如果你没有修改 body 内容，应该使用原始的 body 返回
-$done({ body: $response.body });
