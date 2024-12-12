c984bbf6cf32d938679c044a18ae585cadcca712
yaotutu <957810668@qq.com>
2024-12-12 11:27:02 +0800

    chore: Add Coolapk Image Header Fix script
    The Coolapk Image Header Fix script has been added to the project. This script sets the request header and prints the modified header for debugging purposes.

--- Coolapk-Scripts.js
+++ Coolapk-Scripts.js
@@ -1,1 +1,9 @@
-console.log("Test script has been loaded and executed.");
+// @script type=http-request
+// 在脚本开始时输出调试信息
+console.log("Coolapk Image Header Fix script is running!");
+
+// 设置请求头
+$httpClient.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
+
+// 打印修改后的请求头，方便调试
+console.log($request.headers);
